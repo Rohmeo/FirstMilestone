6be3a50acf933a7789a9eac6a0a7e0434f4f346e
Andrew Rohm <andrew.rohm@duke.edu>
2012-03-19 13:44:24 -0400

    Second Meeting

--- MatrixMultiply.c
+++ MatrixMultiply.c
@@ -11,41 +11,41 @@ main()
 	int i,j;
 	clock_t time1, time2;
 	
-	for(i=1;i<=3;i++)
+	for(i=0;i<=2;i++)
 	{
-		for(j=1;j<=3;j++)
+		for(j=0;j<=2;j++)
 		{
 			Matrix1[i][j]=i+j;
 			Matrix2[i][j]=i*j;
 		}
 	}
 	time1 = clock();
-	for(i=1;i<=3;i++)
+	for(i=0;i<=2;i++)
 	{
-		for(j=1;j<=3;j++)
+		for(j=0;j<=2;j++)
 		{
 		Result[i][j]=matrixProduct(Matrix1,Matrix2,i,j);
 		}
 	}
 	time2 = clock();
 	printf("Matrix 1:\n");
-	for(i=1;i<=3;i++)
+	for(i=0;i<=2;i++)
 	{
-		for(j=1;j<=3;j++)
+		for(j=0;j<=2;j++)
 			printf("%d\t",Matrix1[i][j]);
 		printf("\n");
 	}
 	printf("Matrix 2:\n");
-	for(i=1;i<=3;i++)
+	for(i=0;i<=2;i++)
 		{
-			for(j=1;j<=3;j++)
+			for(j=0;j<=2;j++)
 				printf("%d\t",Matrix2[i][j]);
 			printf("\n");
 		}
 	printf("Result:\n");
-	for(i=1;i<=3;i++)
+	for(i=0;i<=2;i++)
 		{
-			for(j=1;j<=3;j++)
+			for(j=0;j<=2;j++)
 				printf("%d\t",Matrix2[i][j]);
 			printf("\n");
 		}
@@ -56,7 +56,7 @@ int matrixProduct(int Matrix1[3][3], int Matrix2[3][3],int row, int col)
 {
 	int k,sum;
 	sum=0;
-	for(k=1;k<=3;k++)
+	for(k=0;k<=2;k++)
 	{
 		sum=sum+(Matrix1[row][k]*Matrix2[k][col]);
 	}
