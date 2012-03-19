01edce768c5d756bac618d75da028c057f625855
Andrew Rohm <andrew.rohm@duke.edu>
2012-03-19 09:27:02 -0400

    Second Meeting

--- Hello.c
+++ Hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
-  printf("Hello, World!\n");
+	clock_t result1, result2;
+	result1 = clock();
+	printf("Hello, World!\n");
+	result2 = clock();
+	result1 = result2-result1;
+	printf("Elapsed Time:%d \n",result1);
 }
--- MatrixMultiply.c
+++ MatrixMultiply.c
@@ -1,34 +1,64 @@
 #include <stdio.h>
+#include <time.h>
+
+int matrixProduct(int Matrix1[3][3], int Matrix2[3][3], int row, int col);
 
 main()
 {
-	int Matrix1[9];
-	int Matrix2[9];
-	int Result[9];
+	int Matrix1[3][3];
+	int Matrix2[3][3];
+	int Result[3][3];
 	int i,j;
+	clock_t time1, time2;
 	
-	for(i=1;i<=9;++i)
+	for(i=1;i<=3;i++)
 	{
-	        Matrix1[i]=i;
-		Matrix2[i]=i;
+		for(j=1;j<=3;j++)
+		{
+			Matrix1[i][j]=i+j;
+			Matrix2[i][j]=i*j;
+		}
 	}
-	Result[1]=(Matrix1[1]*Matrix2[1])+(Matrix1[2]*Matrix2[4])+(Matrix1[3]*Matrix2[7]);
-	Result[2]=(Matrix1[1]*Matrix2[2])+(Matrix1[2]*Matrix2[5])+(Matrix1[3]*Matrix2[8]);
-	Result[3]=(Matrix1[1]*Matrix2[3])+(Matrix1[2]*Matrix2[6])+(Matrix1[3]*Matrix2[9]);
-	Result[4]=(Matrix1[4]*Matrix2[1])+(Matrix1[5]*Matrix2[4])+(Matrix1[6]*Matrix2[7]);
-	Result[5]=(Matrix1[4]*Matrix2[2])+(Matrix1[5]*Matrix2[5])+(Matrix1[6]*Matrix2[8]);
-	Result[6]=(Matrix1[4]*Matrix2[3])+(Matrix1[5]*Matrix2[6])+(Matrix1[6]*Matrix2[9]);
-	Result[7]=(Matrix1[7]*Matrix2[1])+(Matrix1[8]*Matrix2[4])+(Matrix1[9]*Matrix2[7]);
-	Result[8]=(Matrix1[7]*Matrix2[2])+(Matrix1[8]*Matrix2[5])+(Matrix1[9]*Matrix2[8]);
-	Result[9]=(Matrix1[7]*Matrix2[3])+(Matrix1[8]*Matrix2[6])+(Matrix1[9]*Matrix2[9]);
-	
-	for(j=1;j<=3;++j)
-		printf("%d\t",Result[j]);
-	printf("\n");
-	for(j=4;j<=6;++j)
-		printf("%d\t",Result[j]);
-	printf("\n");
-        for(j=7;j<=9;++j)
-		printf("%d\t",Result[j]);
-	printf("\n");
+	time1 = clock();
+	for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=3;j++)
+		{
+		Result[i][j]=matrixProduct(Matrix1,Matrix2,i,j);
+		}
+	}
+	time2 = clock();
+	printf("Matrix 1:\n");
+	for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=3;j++)
+			printf("%d\t",Matrix1[i][j]);
+		printf("\n");
+	}
+	printf("Matrix 2:\n");
+	for(i=1;i<=3;i++)
+		{
+			for(j=1;j<=3;j++)
+				printf("%d\t",Matrix2[i][j]);
+			printf("\n");
+		}
+	printf("Result:\n");
+	for(i=1;i<=3;i++)
+		{
+			for(j=1;j<=3;j++)
+				printf("%d\t",Matrix2[i][j]);
+			printf("\n");
+		}
+	printf("Elapsed Time:%d\n",time2-time1);
+}
+
+int matrixProduct(int Matrix1[3][3], int Matrix2[3][3],int row, int col)
+{
+	int k,sum;
+	sum=0;
+	for(k=1;k<=3;k++)
+	{
+		sum=sum+(Matrix1[row][k]*Matrix2[k][col]);
+	}
+	return sum;
 }
