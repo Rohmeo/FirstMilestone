452d9467c4e13a9b9a5dd3ad83d4a897ceb58394
Andrew Rohm <mellow.sam@gmail.com>
2012-04-02 11:29:59 -0400

    branched to parallel

---
+++ VectorAddPar.cu
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+__global__ void VecAdd(int *Vec1, int *Vec2, int *Res){
+	Res[threadIdx.x]=Vec1[threadIdx.x]+Vec2[threadIdx.x];
+}
+
+int main(){
+	int length, i;
+	size_t size;
+	int *dev_Vector1;
+	int *dev_Vector2;
+	int *dev_Result;
+	
+	length = 10;
+	size = (length+1)*sizeof(int);
+	
+	int* Vector1 = (int*)malloc(size);
+	int* Vector2 = (int*)malloc(size);
+	int* Result  = (int*)malloc(size);
+	
+	for(i=0;i<=length;i++){
+		Vector1[i] = i;
+		Vector2[i] = i;
+	}
+	cudaMalloc((void**)&dev_Vector1, size);
+	cudaMalloc((void**)&dev_Vector2, size);
+	cudaMalloc((void**)&dev_Result, size);
+	
+	cudaMemcpy(dev_Vector1,Vector1,size,cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_Vector2,Vector2,size,cudaMemcpyHostToDevice);
+	VecAdd<<<length+1,1>>>(dev_Vector1, dev_Vector2, dev_Result);
+	
+	cudaMemcpy(Result, dev_Result, size, cudaMemcpyDeviceToHost);
+	
+	for(i=0;i<=length;i++){
+		printf("%d\t",Result[i]);
+	}
+	printf("\n");
+	return 0;
+}
