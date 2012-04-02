35503ccb2a8331e532038a7490b0d99227878af9
Andrew Rohm <andrew.rohm@duke.edu>
2012-04-01 23:50:50 -0400

    Basic Things

--- Hello.c
+++ Hello.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
+#include <stdlib.h>
 
 int main()
 {
 	struct timeval tv1, tv2;
 	int i;
-	time_t sec, usec;
+	int Matrix[3][3];
+	int *matAddress;
 	
+	int size = 3*3*sizeof(int);
+	
+	matAddress = (int*) malloc(size);
+	
+	
+	time_t sec, usec;
 	gettimeofday(&tv1, NULL);
 	printf("Hello, World!\n");
 	for(i=0;i<=500000;i++);
@@ -16,4 +24,5 @@ int main()
 	usec=tv2.tv_usec-tv1.tv_usec;
 	
 	printf("Elapsed Time: %d \n",usec+(1000000*sec));
+	printf("Integer:%d\n%d\n%d\n",rand(),rand(),rand());
 }
--- ThreadedMatrixMultiply.c
+++ ThreadedMatrixMultiply.c
@@ -2,64 +2,72 @@
 #include <time.h>
 #include <sys/time.h>
 
-int matrixProduct(int Matrix1[3][3], int Matrix2[3][3], int row, int col);
+
+__global__ void matrixProduct(int Matrix1[MatSize][MatSize], int Matrix2[MatSize][MatSize]);
+{
+	int row = threadIdx.x;
+	int col = threadIdx.y;
+	int k,sum;
+	sum=0;
+	for(k=0;k<=MatSize;k++)
+	{
+		sum=sum+(Matrix1[row][k]*Matrix2[k][col]);
+	}
+	Result[row][col]=sum;
+}
+
+
 
 main()
 {
-	int Matrix1[3][3];
-	int Matrix2[3][3];
-	int Result[3][3];
+	//Declare vars, constants
+	int const MatSize=10; 			//Actual dimension is 1+MatSize
+	int Matrix1[MatSize][MatSize];
+	int Matrix2[MatSize][MatSize];
+	int Result[MatSize][MatSize];
 	int i,j;
 	struct timeval start, end;
-	
-	for(i=0;i<=2;i++)
+
+	//Initialize matrices with random values
+	for(i=0;i<=MatSize;i++)
 	{
-		for(j=0;j<=2;j++)
+		for(j=0;j<=MatSize;j++)
 		{
-			Matrix1[i][j]=i+j;
-			Matrix2[i][j]=i*j;
+			Matrix1[i][j]=rand();
+			Matrix2[i][j]=rand();
 		}
 	}
 	gettimeofday(&start,NULL);
-	for(i=0;i<=2;i++)
-	{
-		for(j=0;j<=2;j++)
-		{
-		Result[i][j]=matrixProduct(Matrix1,Matrix2,i,j);
-		}
-	}
+	
+	//Transfer matrices to device memory
+	size_t memSize = MatSize * MatSize * sizeof(int);
+	
+	__loadToShared
+	//Kernel Declaration
+	dim3 blockSize(MatSize,MatSize);
+	dim3 gridSize(1,1);
+	matrixProduct<<<blockSize,gridSize>>>()
 	gettimeofday(&end,NULL);
 	printf("Matrix 1:\n");
-	for(i=0;i<=2;i++)
+	for(i=0;i<=MatSize;i++)
 	{
-		for(j=0;j<=2;j++)
+		for(j=0;j<=MatSize;j++)
 			printf("%d\t",Matrix1[i][j]);
 		printf("\n");
 	}
 	printf("Matrix 2:\n");
-	for(i=0;i<=2;i++)
+	for(i=0;i<=MatSize;i++)
 		{
-			for(j=0;j<=2;j++)
+			for(j=0;j<=MatSize;j++)
 				printf("%d\t",Matrix2[i][j]);
 			printf("\n");
 		}
 	printf("Result:\n");
-	for(i=0;i<=2;i++)
+	for(i=0;i<=MatSize;i++)
 		{
-			for(j=0;j<=2;j++)
+			for(j=0;j<=MatSize;j++)
 				printf("%d\t",Matrix2[i][j]);
 			printf("\n");
 		}
 	printf("Elapsed Time: %d \n",/*((end.tv_sec-start.tv_sec)*1000000)+*/(end.tv_usec-start.tv_usec));
-}
-
-int matrixProduct(int Matrix1[3][3], int Matrix2[3][3],int row, int col)
-{
-	int k,sum;
-	sum=0;
-	for(k=0;k<=2;k++)
-	{
-		sum=sum+(Matrix1[row][k]*Matrix2[k][col]);
-	}
-	return sum;
 }
